5e17f161cbc4288bb4ad51d2aa5486c2c2ab7569
SuperMayy <mayowashowumi@outlook.com>
2020-09-09 17:03:21 +0100

    Commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//nav bar
 var x = document.getElementById("theSidebar");
 var y = document.getElementById("theDeleteButton");
 var z = document.getElementById("theBar");
@@ -28,3 +29,44 @@ function displayNavbar() {
         z.style.display = "block"; 
     }
   }
+
+// the changeing text
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["sarah stephenson", "a copywriter", "a scriptwriter"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if(charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing"))cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if(charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex >=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  if(textArray.length)setTimeout(type, newTextDelay + 250);
+});
